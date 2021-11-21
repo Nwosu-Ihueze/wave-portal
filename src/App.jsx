70f2ecaaf843caafc7da0206d166b8a03dd861fe
Nwosu-Ihueze <nwosunneomagmail.com>
2021-11-21 13:01:43 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,264 @@
+
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import abi from "./utils/WavePortal.json";
+import "./App.css";
+
+export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [numWaves, setNumWaves] = useState(0);
+  const [allWaves, setAllWaves] = useState([]);
+
+  const contractAddress = "0x72Bac50626a61BE164BE91A4716af332FB505D15";
+  const contractABI = abi.abi;
+
+  const getAllWaves = async () => {
+    const { ethereum } = window;
+
+    try {
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        const waves = await wavePortalContract.getAllWaves();
+        const wavesCleaned = waves.map((wave) => {
+          return {
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message,
+          };
+        });
+
+        setAllWaves(wavesCleaned.reverse());
+        setNumWaves(wavesCleaned.length);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getAllWaves();
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const wave = async () => {
+    setLoading(true);
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        setSuccess(true);
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        const waveTxn = await wavePortalContract.wave(message, {
+          gasLimit: 300000,
+        });
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        const waves = await wavePortalContract.getAllWaves().length;
+        setNumWaves(waves);
+        setLoading(false);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setSuccess(false);
+    }
+  };
+
+  useEffect(() => {
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves((prevState) =>
+        [
+          ...prevState,
+          {
+            address: from,
+            timestamp: new Date(timestamp * 1000),
+            message: message,
+          },
+        ].reverse()
+      );
+      setSuccess(false);
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        signer
+      );
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
+  }, [contractABI]);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  useEffect(() => {
+    if (message.length > 2) setDisabled(false);
+  }, [message]);
+
+  return (
+    <div className="mainContainer">
+      {numWaves ? (
+        <div className="number">
+          Number of messages sent to me: <span className="num">{numWaves}</span>
+        </div>
+      ) : null}
+
+      {success && <div className="thanks">Thank you!</div>}
+
+      <div className="dataContainer">
+        <div className="header">Lee Anya! (Hey There)</div>
+
+        <div className="bio">
+          Aha m bu Rosemary. A na m agu akwukwo na mahadum di na Kentucky. A na m amu ka esi eji igwe mmuta imeputa nka. Jikoo obere akpa Ethereum gi feere m aka! <br/><br/>
+
+        (My name is Rosemary. I school at a University in Kentucky. I'm learning how to use machine learning in arts. Connect your Ethereum wallet and wave at me!)
+          {currentAccount && (
+            <form className="message" onSubmit={wave}>
+              <input
+                required
+                className="input"
+                type="text"
+                name="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Write your message here."
+              />
+
+              <input
+                type="submit"
+                className="waveButton"
+                disabled={disabled}
+                value={loading ? "Loading..." : "Send"}
+              />
+            </form>
+          )}
+        </div>
+
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+        {currentAccount && (
+          <>
+            <div className="bio">Previously Sent Messages:</div>
+            <div className="messages">
+              {allWaves.map((wave, index) => {
+                return (
+                  <div key={index} style={{ marginBottom: "8px" }}>
+                    <div>Address: {wave.address}</div>
+                    <div>Time: {wave.timestamp.toString()}</div>
+                    <div>
+                      Message: <b>{wave.message}</b>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+
+      <footer className="photoCredit">
+        <p>
+          Follow AdaIhueze on{" "}
+          <a
+            href="https://github.com/Nwosu-Ihueze/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </p>
+        <p>
+          Or
+          on{" "}
+          <a href="https://twitter.com/adaihueze">
+            Twitter
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
